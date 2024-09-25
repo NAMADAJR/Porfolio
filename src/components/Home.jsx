69c5ecdd7916e14d4ff247c6c87bb5e2e4f8b4ef
Namada Junior <namada871@gmail.com>
2024-09-25 09:46:58 +0300

    Adds prealoder to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import Preloader from './Preloader/Preloader'
 
 const Home = () => {
   return (
 
     <div>
+      <Preloader />
       <Header />
       <h1>Home Page</h1>
       </div>
